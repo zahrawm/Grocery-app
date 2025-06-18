96db910dfcbe4ddfc91449bfac7ad1a5b20f974d
Fatimah Zahra Adam <92226557+zahrawm@users.noreply.github.com>
2025-06-18 11:59:04 +0000

    codes updated

---
+++ grocery_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/screen/home_screen.dart' show HomeScreen;
+import 'package:grocery_app/screen/login_screen.dart';
+import 'package:grocery_app/screen/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const SplashScreen(),
+        '/login': (_) => const LoginScreen(),
+        '/home': (_) => const HomeScreen(),
+      },
+      
+    );
+  }
+}
+
---
+++ grocery_app/lib/screen/splash_screen.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+import 'package:grocery_app/screen/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {});
+    });
+
+    Future.delayed(const Duration(seconds: 3), () async {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Stack(
+        children: [
+          Center(
+            child: SizedBox(
+              height: 240,
+              width: 240,
+              child: Image.asset("assets/bo.jpg", fit: BoxFit.cover),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
